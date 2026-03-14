2e41ca82fc7cef707b6591f6a2cb647188b064fa
Ade Saputra <see2316434@gmail.com>
2026-03-14 00:17:37 +0000

    UI rusak dan menyempit

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -3,9 +3,9 @@ import Sidebar from '@/components/layout/sidebar';
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+    <div className="flex min-h-screen w-full">
       <Sidebar />
-      <div className="flex flex-col">
+      <div className="flex flex-1 flex-col sm:pl-14">
         <Header />
         <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 bg-background">
           {children}
